cdc5c40353b9499163fb98abb1768960ca231b92
chaitumadhu <chaitanyakrp@gmail.com>
2025-04-12 23:19:27 +0530

    Initial commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("Hello world");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
