c45482e632225d8939dfc16f966e73fb6871e04a
chaitumadhu <chaitanyakrp@gmail.com>
2025-04-12 23:20:42 +0530

    removed syso

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("Hello world");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
